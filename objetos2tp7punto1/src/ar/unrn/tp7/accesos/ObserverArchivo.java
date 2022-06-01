c3b258520998c14c4b2a53d676362e1248739932
Vainillas <maliberti@unrn.edu.ar>
2022-06-01 18:17:37 -0300

    punto1

---
+++ objetos2tp7punto1/src/ar/unrn/tp7/accesos/ObserverArchivo.java
@@ -0,0 +1,28 @@
+package ar.unrn.tp7.accesos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+
+import ar.unrn.tp7.modelo.Observer;
+
+public class ObserverArchivo implements Observer {
+
+	private String pathArchivo;
+
+	@Override
+	public void actualizar(String temperatura) {
+
+		try {
+			FileWriter escritorArchivo = new FileWriter("C:\\Users\\maliberti\\Downloads\\Temperaturas.txt",
+					true);
+			escritorArchivo.write(temperatura + "�, Fecha de Realizacion: " + LocalDate.now() + "\r\n");
+			escritorArchivo.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
